36c7ed4da552237053fb395e21c71497d4e729fd
bibashmanjusubedi <acharabibash@outlook.com>
2025-02-09 20:07:53 +0545

    Day 3 Complete
    Day 4 Remaining

--- Day3/img_blurring.py
+++ Day3/img_blurring.py
@@ -14,17 +14,12 @@ if not os.path.exists(output_dir):
 
 # importing feature extraction classes
 from images_to_matrix import images_to_matrix_class
-# from images_matrix_for_2d_square_pca import  images_to_matrix_class_for_two_d
+
 from dataset import dataset_class
 
-# Algo Type (pca, 2d-pca, 2d2-pca)
+# Algo Type
 algo_type = "pca"
 
-
-# #for single image = 0
-# #for video = 1
-# #for group image = 2
-# reco_type = 0
 
 #No of images For Training(Left will be used as testing Image)
 no_of_images_of_one_person = 8
